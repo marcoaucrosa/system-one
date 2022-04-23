bc14ab07cf5ad7d97007bbc153a1c8e6aba731c4
Marco Rosa <marcocarneirorosa@gmail.com>
2022-04-23 12:00:21 -0300

    Inicio de projeto

---
+++ src/main/java/br/com/marco/systemone/SystemoneApplication.java
@@ -0,0 +1,13 @@
+package br.com.marco.systemone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemoneApplication.class, args);
+	}
+
+}
